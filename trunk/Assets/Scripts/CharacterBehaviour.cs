67fffea5d423d703b6142174f0cd6c1cdb823c14
Stjoopa@gmail.com <Stjoopa@gmail.com@4cdbfa57-ea99-4105-3104-43fa04224b76>
2012-11-11 21:17:18 +0000

    Moved the environment for easier character manipulation. Simple character movement controls with gravity and collision.

--- trunk/Assets/Scripts/CharacterBehaviour.cs
+++ trunk/Assets/Scripts/CharacterBehaviour.cs
@@ -2,18 +2,52 @@ using UnityEngine;
 using System.Collections;
 
 public class CharacterBehaviour : MonoBehaviour {
+	
+	public GameObject playerGameObject;
+	public CharacterController playerCharacterController;
 
 	// Use this for initialization
 	void Start () {
-		GameObject refGameObject = GameObject.Find("PlayerCharacterSpriteManager");
-		SpriteManager mySpriteManager = (SpriteManager)refGameObject.GetComponent("LinkedSpriteManager");
-		
-		GameObject playerGameObject = GameObject.Find ("PlayerCharacterObject");
-		mySpriteManager.AddSprite(playerGameObject,1f, 1f, 0,0,64,128,true);
+		playerGameObject = GameObject.Find("PlayerCharacterObject");
+		playerCharacterController = (CharacterController)playerGameObject.GetComponent("CharacterController");
+		drawCharacterSprite();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 moveDirection = Vector3.zero;
+		
+		if(playerCharacterController.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	        moveDirection = transform.TransformDirection(moveDirection);
+        	moveDirection *= 10;
+		}
+		moveDirection.y -= 20 * Time.deltaTime;
+		playerCharacterController.Move(moveDirection * Time.deltaTime);
+		/*if(Input.GetButton("Vertical")) {
+			Vector3 direction;
+			if(Input.GetAxis("Vertical") > 0) {
+				direction = new Vector3(0,0,speed);
+			} else {
+				direction = new Vector3(0,0,-speed);
+			}
+			playerCharacterController.Move(direction);
+		}
+		if(Input.GetButton ("Horizontal")) {
+			Vector3 direction;
+			if(Input.GetAxis("Horizontal") > 0) {
+				direction = new Vector3(speed,0,0);
+			} else {
+				direction = new Vector3(-speed,0,0);
+			}
+			playerCharacterController.Move(direction);
+		}*/
+	}
 	
+	void drawCharacterSprite() {
+		GameObject refGameObject = GameObject.Find("PlayerCharacterSpriteManager");
+		SpriteManager mySpriteManager = (SpriteManager)refGameObject.GetComponent("LinkedSpriteManager");
+		
+		mySpriteManager.AddSprite(playerGameObject,4f,4f, 0,0,64,128,true);
 	}
 }
