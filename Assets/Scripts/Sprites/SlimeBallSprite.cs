08c9b888664da8898fba9c851e006df8a6704077
Stjoopa@gmail.com <Stjoopa@gmail.com@4cdbfa57-ea99-4105-3104-43fa04224b76>
2013-01-11 21:19:45 +0000

    Updated sprites, no longer showing white borders

--- Assets/Scripts/Sprites/SlimeBallSprite.cs
+++ Assets/Scripts/Sprites/SlimeBallSprite.cs
@@ -6,7 +6,7 @@ public class SlimeBallSprite : BaseSprite {
 	protected override void DefineSpriteAnimations() {
 		Vector2 spriteUVSize = spriteManager.PixelSpaceToUVSpace(32,32);
 		
-		AddAnimation("move", spriteUVSize, 0, 32, 5);
-		AddAnimation("idle", spriteUVSize, 0, 32, 1);
+		AddAnimation("move", spriteUVSize, 32, 48, 5);
+		AddAnimation("idle", spriteUVSize, 32, 48, 1);
 	}
 }
