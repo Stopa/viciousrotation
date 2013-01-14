04143dcf052bd0ae4a834eaac1e603010109a245
Stjoopa@gmail.com <Stjoopa@gmail.com@4cdbfa57-ea99-4105-3104-43fa04224b76>
2013-01-14 21:58:46 +0000

    Defined animations for Zombie sprite, also some .meta files

---
+++ Assets/Scripts/Sprites/ZombieSprite.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZombieSprite : BaseSprite {
+
+	protected override void DefineSpriteAnimations() {
+		Vector2 spriteUVSize = spriteManager.PixelSpaceToUVSpace(64,96);
+		
+		AddAnimation("walk_l", spriteUVSize, 0,   96, 5);
+		AddAnimation("walk_r", spriteUVSize, 320, 96, 5);
+		
+		AddAnimation("melee_l", spriteUVSize, 128, 192, 3, false);
+		AddAnimation("melee_r", spriteUVSize, 320, 192, 3, false);
+		
+		AddAnimation("death_l", spriteUVSize, 0, 288, 8, false);
+		AddAnimation("death_r", spriteUVSize, 0, 384, 8, false);
+	}
+}
