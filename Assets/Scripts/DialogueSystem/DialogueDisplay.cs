fd7c9ce373aac38c0b0bf7c380300524897403dc
sikakurr@gmail.com <sikakurr@gmail.com@4cdbfa57-ea99-4105-3104-43fa04224b76>
2013-01-28 13:52:08 +0000

    Dialogue Screen GUI stuff

--- Assets/Scripts/DialogueSystem/DialogueDisplay.cs
+++ Assets/Scripts/DialogueSystem/DialogueDisplay.cs
@@ -6,11 +6,14 @@ public class DialogueDisplay : MonoBehaviour {
 	public string[] selStrings;
 	private PlayerCharacter _player;
 	private FriendlyCharacter _partner;
+		
+	private GUISkin _customSkin;
 	
 	// Use this for initialization
 	void Start () {
 		_showDialogue = false;
 		_player = gameObject.GetComponent("PlayerCharacter") as PlayerCharacter;
+		_customSkin = Resources.Load("Gui/DialogueSkin") as GUISkin;
 	}
 	
 	void Update() {
@@ -24,18 +27,21 @@ public class DialogueDisplay : MonoBehaviour {
 		int selGridInt = -1;
 		selStrings = new string[] {"Ambiguous choice 1", "Ambiguous choice 2", "Ambiguous choice 3", "Ambiguous choice 4"};
 		if(_showDialogue) {
-		    GUI.BeginGroup(new Rect(0, Screen.height - 200, Screen.width, 200));
-		
+			GUI.skin = _customSkin;
+			
+		    GUI.BeginGroup(new Rect(0, Screen.height - 200, Screen.width, 200));		
 		    // Draw a box in the new coordinate space defined by the BeginGroup.
 			GUI.Box(new Rect(0,0,180,200), "PORTRAIT YOU");
 			GUI.DrawTexture(new Rect(0,0,180,200), _player._portrait);
 			GUI.Box(new Rect(Screen.width - 180,0,180,200), "PORTRAIT OTHER GUY");
 			GUI.DrawTexture(new Rect(Screen.width - 180,0,180,200), _partner._portrait);
-			GUI.Box(new Rect(180,0,Screen.width - 180*2,100), "HE SAYS THIS");
-			selGridInt = GUI.SelectionGrid(new Rect(180, 100, Screen.width - 180*2, 100), selGridInt, selStrings, 2);	
-			GUI.EndGroup();
 			
-			CheckSelection(selGridInt);			
+			GUI.Box(new Rect(180,0,Screen.width - 180*2,100), "HE SAYS THIS TEXT");
+			selGridInt = GUI.SelectionGrid(new Rect(180, 100, Screen.width - 180*2, 100), selGridInt, selStrings, 1);	
+			GUI.EndGroup();			
+			CheckSelection(selGridInt);
+			
+			GUI.skin = null;			
 		}
 	}
 	
